134f0794f9b09b48b6d160e01684b9741eb64236
João Paulo Dias <joaopdiasventura@gmail.com>
2024-04-21 17:37:28 -0300

    feat: verificando se a data é válida

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ function parseDate(input: string): Date {
 
 function findZodiacSign(dateInput: string): ChineseZodiacSign | undefined {
   const date = parseDate(dateInput);
+  if (isNaN(date.getTime())) {
+    alert("DIGITE UMA DATA VÁLIDA");
+    return;
+  }
 
   for (const sign of chineseZodiac) {
     for (const range of sign.dateRange) {
@@ -43,12 +47,18 @@ function App() {
           className="bg-transparent focus:ring-0 flex-1 rounded-lg outline-none border p-1 text-sm select-text"
           required
         />
-        <input type="submit" className="border rounded-lg cursor-pointer" value="BUSCAR" />
+        <input
+          type="submit"
+          className="border rounded-lg cursor-pointer"
+          value="BUSCAR"
+        />
       </form>
       <div className="flex flex-col gap-1">
         <h3>{"name" in Sign ? Sign.name : "Nenhum"}</h3>
         <p>
-          {"characteristics" in Sign ? `Características: ${Sign.characteristics}` : ""}
+          {"characteristics" in Sign
+            ? `Características: ${Sign.characteristics}`
+            : ""}
         </p>
       </div>
     </div>
